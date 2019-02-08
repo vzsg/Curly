e9128ef17686c43777bfbfc2fe07fd78b7b1def6
Zsolt Varadi <vzsg@users.noreply.github.com>
2019-02-08 10:26:42 +0100

    Add a get example test for manual introspection

--- Tests/CurlyClientTests/CurlyClientTests.swift
+++ Tests/CurlyClientTests/CurlyClientTests.swift
@@ -9,12 +9,30 @@ final class CurlyClientTests: XCTestCase {
 
         var http = HTTPRequest(method: .POST, url: "https://httpbin.org/post")
         http.headers.replaceOrAdd(name: "X-Test-Header", value: "Foo")
+
         let req = Request(http: http, using: app)
+        try req.query.encode(["q1": "baz", "q2": "dm1"])
 
         try req.content.encode([
             "foo": "bar",
             "baz": "qux"
-        ], as: .json)
+        ], as: MediaType.urlEncodedForm)
+
+        let client = CurlyClient(on: app)
+        let res = try client.send(req).wait()
+
+        XCTAssertNotNil(res.http.body.data)
+        XCTAssert(!Array(res.http.headers).isEmpty)
+    }
+
+    func testHttpBinGet() throws {
+        let app = try Application()
+
+        var http = HTTPRequest(method: .GET, url: "https://httpbin.org/get")
+        http.headers.replaceOrAdd(name: "X-Test-Header", value: "Foo")
+
+        let req = Request(http: http, using: app)
+        try req.query.encode(["q1": "baz", "q2": "dm1"])
 
         let client = CurlyClient(on: app)
         let res = try client.send(req).wait()
@@ -25,5 +43,6 @@ final class CurlyClientTests: XCTestCase {
 
     static var allTests = [
         ("testHttpBinPost", testHttpBinPost),
+        ("testHttpBinGet", testHttpBinGet),
     ]
 }
